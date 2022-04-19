0fdaf9e93c575d257e14bfb7759ef8b406cd4403
Tiago Schmidt <schmidttiago7@gmail.com>
2022-04-19 09:49:10 -0300

    Refatoração geral. Classes simplificadas e melhoradas.

--- src/Triangle.java
+++ src/Triangle.java
@@ -3,33 +3,36 @@ public class Triangle {
 	private double aSide;
 	private double bSide;
 	private double cSide;
-	
-	Triangle(double sideA, double sideB, double sideC){
-		this.aSide = sideA;
-		this.bSide = sideB;
-		this.cSide = sideC;
+
+	Triangle(double sideA, double sideB, double sideC) {
+		aSide = sideA;
+		bSide = sideB;
+		cSide = sideC;
 	}
-	
-	public String checkTriangle() {
-		String returnString;	
-		
-		if((aSide < 0) || (bSide < 0) || (cSide < 0)) {
-			returnString = "INVALID";		
-		}else {
-			if((aSide + bSide > cSide) && (aSide + cSide > bSide) && (cSide + bSide > aSide)) {
-				if((aSide == bSide) && (aSide == cSide)) {				
-					returnString = "EQUILATERAL";
-				}else if((aSide == bSide) || (aSide == cSide) || (bSide == cSide)) {
-					returnString = "ISOSCELES";
-				}else {
-					returnString = "SCALENE";
-				}
-			}else {
-				returnString = "INVALID";
-			}
-			
+
+	public boolean isValid() {
+		return checkNegative() && checkSides();
+	}
+
+	public String checkType() {
+		String returnString;
+
+		if ((aSide == bSide) && (aSide == cSide)) {
+			returnString = "EQUILATERAL";
+		} else if ((aSide == bSide) || (aSide == cSide) || (bSide == cSide)) {
+			returnString = "ISOSCELES";
+		} else {
+			returnString = "SCALENE";
 		}
-		
+
 		return returnString;
+	}
+
+	private boolean checkNegative() {
+		return aSide > 0 && bSide > 0 && cSide > 0;
+	}
+
+	private boolean checkSides() {
+		return (aSide + bSide > cSide) && (aSide + cSide > bSide) && (cSide + bSide > aSide);
 	}
 }
--- src/TriangleTests.java
+++ src/TriangleTests.java
@@ -1,4 +1,6 @@
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -6,73 +8,84 @@ import org.junit.jupiter.api.Test;
 class TriangleTests {
 
 	@Test
-	@DisplayName("Testes de Triângulo Escaleno válido.")
+	@DisplayName("Teste de Triângulo Escaleno válido.")
 	void testValidScalene() {
-		Triangle t = new Triangle(11,12,13);
-		assertEquals(t.checkTriangle(),"SCALENE");
+		Triangle t = new Triangle(11, 12, 13);
+		assertTrue(t.isValid());
+		assertEquals(t.checkType(), "SCALENE");
 	}
-	
+
 	@Test
-	@DisplayName("Testes de Triângulo Isósceles válido.")
+	@DisplayName("Teste de Triângulo Isósceles válido.")
 	void testValidIsosceles() {
-		Triangle t = new Triangle(11,11,13);
-		assertEquals(t.checkTriangle(),"ISOSCELES");
+		Triangle t = new Triangle(11, 11, 13);
+		assertTrue(t.isValid());
+		assertEquals(t.checkType(), "ISOSCELES");
 	}
-	
+
+	@Test
+	@DisplayName("Teste de Triângulo Equilátero válido.")
+	void testValidEquilateral() {
+		Triangle t = new Triangle(11, 11, 11);
+		assertTrue(t.isValid());
+		assertEquals(t.checkType(), "EQUILATERAL");
+	}
+
 	@Test
 	@DisplayName("Testes de Triângulo Isósceles Válido Iterado.")
 	void testValidIsoscelesIteration() {
-		int sides[][] = {{11,11,12},{11,12,11},{12,11,11}};
+		int sides[][] = { { 11, 11, 12 }, { 11, 12, 11 }, { 12, 11, 11 } };
 		int i;
-		
-		for(i=0;i<sides.length;i++) {
-			Triangle t = new Triangle(sides[i][0],sides[i][1],sides[i][2]);
-			assertEquals(t.checkTriangle(),"ISOSCELES");
+
+		for (i = 0; i < sides.length; i++) {
+			Triangle t = new Triangle(sides[i][0], sides[i][1], sides[i][2]);
+			assertTrue(t.isValid());
+			assertEquals(t.checkType(), "ISOSCELES");
 		}
 	}
-	
+
+	@Test
+	@DisplayName("Teste de Zero.")
+	void testZero() {
+		Triangle t = new Triangle(12, 16, 0);
+		assertFalse(t.isValid());
+	}
+
+	@Test
+	@DisplayName("Teste de Negativos.")
+	void testNegative() {
+		Triangle t = new Triangle(-10, -10, -10);
+		assertFalse(t.isValid());
+	}
+
 	@Test
 	@DisplayName("Testes de Triângulo com a soma sendo igual ao terceiro.")
 	void testValidCEqualsSum() {
-		int sides[][] = {{7,5,12},{5,12,7},{12,7,5}};
+		int sides[][] = { { 7, 5, 12 }, { 5, 12, 7 }, { 12, 7, 5 } };
 		int i;
-		
-		for(i=0;i<sides.length;i++) {
-			Triangle t = new Triangle(sides[i][0],sides[i][1],sides[i][2]);
-			assertEquals(t.checkTriangle(),"INVALID");
+
+		for (i = 0; i < sides.length; i++) {
+			Triangle t = new Triangle(sides[i][0], sides[i][1], sides[i][2]);
+			assertFalse(t.isValid());
 		}
 	}
-	
-	@Test
-	@DisplayName("Testes de Triângulo Equilátero válido.")
-	void testValidEquilateral() {
-		Triangle t = new Triangle(11,11,11);
-		assertEquals(t.checkTriangle(),"EQUILATERAL");
-	}
-	
-	@Test
-	@DisplayName("Testes de Zeros.")
-	void testZeros() {
-		Triangle t = new Triangle(0,0,0);
-		assertEquals(t.checkTriangle(),"INVALID");
-	}
-	
-	@Test
-	@DisplayName("Testes de Negativos.")
-	void testNegative() {
-		Triangle t = new Triangle(-10,-10,-10);
-		assertEquals(t.checkTriangle(),"INVALID");
-	}
-	
+
 	@Test
 	@DisplayName("Testes de Triângulo com a soma sendo menor que o terceiro.")
 	void testValidCSmallerSum() {
-		int sides[][] = {{7,4,12},{4,12,7},{12,7,4}};
+		int sides[][] = { { 7, 4, 12 }, { 4, 12, 7 }, { 12, 7, 4 } };
 		int i;
-		
-		for(i=0;i<sides.length;i++) {
-			Triangle t = new Triangle(sides[i][0],sides[i][1],sides[i][2]);
-			assertEquals(t.checkTriangle(),"INVALID");
+
+		for (i = 0; i < sides.length; i++) {
+			Triangle t = new Triangle(sides[i][0], sides[i][1], sides[i][2]);
+			assertFalse(t.isValid());
 		}
+	}
+
+	@Test
+	@DisplayName("Teste de Zeros.")
+	void testZeros() {
+		Triangle t = new Triangle(0, 0, 0);
+		assertFalse(t.isValid());
 	}
 }
